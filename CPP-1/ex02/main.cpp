e0b48551a22c3accebc9f4beeaf9a57fbc901018
lchauvet <lchauvet@c1r1p6.42lehavre.fr>
2025-01-24 11:27:28 +0100

    add: CPP-1/e02: finish

---
+++ CPP-1/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchauvet <lchauvet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 11:07:21 by lchauvet          #+#    #+#             */
+/*   Updated: 2025/01/24 11:20:41 by lchauvet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <iomanip>
+
+int main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &str;
+	std::string&	stringREF = str;
+
+	std::cout << "memory address of:" << '\n';
+	std::cout << std::setw(15) << "str: "<< &str << '\n';
+	std::cout << std::setw(15) << "stringPTR: "<< stringPTR << '\n';
+	std::cout << std::setw(15) << "stringREF: "<< &stringREF << "\n\n";
+
+	std::cout << "value of:" << '\n';
+	std::cout << std::setw(15) << "str: "<< str << '\n';
+	std::cout << std::setw(15) << "stringPTR: "<< *stringPTR << '\n';
+	std::cout << std::setw(15) << "stringREF: "<< stringREF << '\n';
+	return (0);
+}
