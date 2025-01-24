097ed281aeac9930d8d0a59cb7daad86b89d8d20
lchauvet <lchauvet@c1r1p6.42lehavre.fr>
2025-01-24 10:55:36 +0100

    end: CPP-1/ex01

--- CPP-1/ex01/main.cpp
+++ CPP-1/ex01/main.cpp
@@ -6,19 +6,26 @@
 /*   By: lchauvet <lchauvet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:18:01 by lchauvet          #+#    #+#             */
-/*   Updated: 2025/01/23 17:57:01 by lchauvet         ###   ########.fr       */
+/*   Updated: 2025/01/24 10:54:43 by lchauvet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cstdlib>
 #include "Zombie.hpp"
 
 int main(void)
 {
 	Zombie*	zombie;
+	int nb_of_zombie = 50000;
 
-	randomChump(("Cap'n amoraru"));
-	zombie = newZombie("Baptiste");
-	zombie->announce();
-	delete zombie;
+	zombie = zombieHorde(nb_of_zombie, "Cap'n amoraru");
+	if (!zombie)
+		return (EXIT_FAILURE);
+	for (int i = 0; i < nb_of_zombie; i++)
+	{
+		zombie[i].announce();
+	}
+	
+	delete [] zombie;
 	return (0);
 }
--- CPP-1/ex01/zombieHorde.cpp
+++ CPP-1/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: lchauvet <lchauvet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:58:13 by lchauvet          #+#    #+#             */
-/*   Updated: 2025/01/23 17:59:02 by lchauvet         ###   ########.fr       */
+/*   Updated: 2025/01/24 10:53:34 by lchauvet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 Zombie* zombieHorde(int N, std::string name)
 {
-
+	if (N <= 0)
+		return (NULL);
+	Zombie*	zombiehorde = new Zombie[N];
+	if (!zombiehorde)
+		return (NULL);
+	for (int i = 0; i < N; i++)
+		zombiehorde[i].set_name(name);
+	return (zombiehorde);
 }
