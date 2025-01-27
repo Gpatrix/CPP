e94117abcab972c767c5d7ec91312eb83b7b1a0d
lchauvet <lchauvet@c1r1p6.42lehavre.fr>
2025-01-27 13:06:41 +0100

    end: CPP-1/ex04

---
+++ CPP-1/ex04/losers.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   losers.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchauvet <lchauvet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 11:41:42 by lchauvet          #+#    #+#             */
+/*   Updated: 2025/01/27 12:15:22 by lchauvet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+#define ERROR_OPEN_FILE	"Error: could not open file"
+#define ERROR_ARGS		"Error: usage: ./losers filename s1 s2"
--- CPP-1/ex04/main.cpp
+++ CPP-1/ex04/main.cpp
@@ -6,19 +6,79 @@
 /*   By: lchauvet <lchauvet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 12:56:58 by lchauvet          #+#    #+#             */
-/*   Updated: 2025/01/24 14:10:33 by lchauvet         ###   ########.fr       */
+/*   Updated: 2025/01/27 13:03:38 by lchauvet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <cstdlib>
+#include "losers.hpp"
 
-int main(int argc, char const **argv)
+bool	get_content (std::string& content, char* filename)
 {
-	if (argc != 4)
+	std::ifstream	InFile;
+	char		c;
+
+	InFile.open(filename, std::ios::in);
+	if (!InFile.is_open())
+	{
+		std::cout << ERROR_OPEN_FILE << " \'" << filename << "\'\n";
 		return (EXIT_FAILURE);
-	std::string	filename = argv[1];
-	std::string	s1 = argv[2];
-	std::string	s2 = argv[3];
-	return (0);
+	}
+	while (!InFile.eof())
+	{
+		InFile >> std::noskipws >> c;
+		content += c;
+	}
+	InFile.close();
+	return (EXIT_SUCCESS);
+}
+
+void	find_and_replace(std::string& content, std::string s1, std::string s2)
+{
+	std::size_t	pos = 0;
+	
+	while (pos != content.npos)
+	{
+		pos = content.find(s1, pos);
+		if (pos != content.npos)
+		{
+			content.erase(pos, s1.length());
+			content.insert(pos, s2);
+		}
+	}
+}
+
+bool	copie_content(std::string& content, std::string filename)
+{
+	std::ofstream	OutFile;
+
+	OutFile.open((filename + ".replace").c_str(), std::ios::out | std::ios::trunc);
+	if (!OutFile.is_open())
+	{
+		std::cout << ERROR_OPEN_FILE << " \'" << filename << "\'\n";
+		return (EXIT_FAILURE);
+	}
+	OutFile << content;
+	OutFile.close();
+	return (EXIT_SUCCESS);
+}
+
+int main(int argc, char **argv)
+{
+	std::ofstream	OutFile;
+	std::string		content;
+
+	if (argc != 4
+		|| argv[1][0] == '\0'
+		|| argv[2][0] == '\0'
+		|| argv[3][0] == '\0')
+	{
+		std::cout << ERROR_ARGS << '\n';
+		return (EXIT_FAILURE);
+	}
+
+	if (get_content(content, argv[1]))
+		return (EXIT_FAILURE);
+	find_and_replace(content, argv[2], argv[3]);
+	copie_content(content, argv[1]);
+	return (EXIT_SUCCESS);
 }
