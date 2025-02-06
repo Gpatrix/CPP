2b10856b75a00ec2bfbfde8e76ccd034d951cfc5
Gap <NaN>
2025-02-06 13:23:31 +0100

    fix: CPP0

--- CPP-0/ex01/main.cpp
+++ CPP-0/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lchauvet <lchauvet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 10:27:42 by lchauvet          #+#    #+#             */
-/*   Updated: 2025/01/23 16:09:17 by lchauvet         ###   ########.fr       */
+/*   Updated: 2025/02/06 13:22:52 by lchauvet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int main(void)
 
 	while (true)
 	{
+		if (std::cin.eof())
+			return (EXIT_SUCCESS);
 		std::cout << "Enter your command > ";
 		getline(std::cin, input);
 		if (std::cin.eof() || input == "EXIT")
