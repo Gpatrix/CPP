b89db45a1c7be373f33bc1b8e6ab750ed2b5055a
Gap <NaN>
2025-02-12 08:40:40 +0100

    refacto: CPP-0/ex00

--- CPP-2/ex00/Fixed.hpp
+++ CPP-2/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: lchauvet <lchauvet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:03:40 by lchauvet          #+#    #+#             */
-/*   Updated: 2025/01/27 15:37:42 by lchauvet         ###   ########.fr       */
+/*   Updated: 2025/02/11 16:44:43 by lchauvet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,16 @@
 
 class Fixed
 {
-private:
-	int					value;
-	static const int	bitNb = 8;
-public:
-	Fixed(void);
-	Fixed(const Fixed&);
-	Fixed&	operator=(const Fixed&);
-	~Fixed();
+	private:
+		int					value;
+		static const int	bitNb = 8;
 
-	int		getRawBits(void) const;
-	void	setRawBits(int const raw);
+	public:
+		Fixed(void);
+		Fixed(const Fixed&);
+		Fixed&	operator=(const Fixed&);
+		~Fixed();
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
 };
