2f44bab1b026a29d0ada41a7cd3ef702dce2b1ac
Gap <NaN>
2025-02-18 08:45:28 +0100

    fix: CPP-4/ex01: del useless test

--- CPP-4/ex01/main.cpp
+++ CPP-4/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lchauvet <lchauvet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 15:09:14 by lchauvet          #+#    #+#             */
-/*   Updated: 2025/01/30 11:08:53 by lchauvet         ###   ########.fr       */
+/*   Updated: 2025/02/18 08:44:49 by lchauvet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,6 @@
 
 int main(void)
 {
-	const Animal* dog = new Dog();
-	const Animal* cat = new Cat();
-
-	const Dog* dog_solo = new Dog();
-	const Cat* cat_solo = new Cat();
-
-	delete dog;
-	delete cat;
-	delete dog_solo;
-	delete cat_solo;
-
 	Animal* animals[100];
 
 	for (short i = 0; i < 100; i += 2)
