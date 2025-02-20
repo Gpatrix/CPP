0e344ff4cf4284d25cb2bdcfc9b1d0470ac95015
Gap <132090220+Gpatrix@users.noreply.github.com>
2025-02-20 12:42:52 +0100

    add|finish: CPP-6/ex02

--- CPP-6/ex02/main.cpp
+++ CPP-6/ex02/main.cpp
@@ -29,12 +29,59 @@ Base * generate(void)
 	return (NULL);
 }
 
-void identify(Base* p);
+void identify(Base* p)
+{
+	A* new_A = dynamic_cast<A*>(p);
+	if (new_A != NULL)
+	{
+		std::cout << "pointer is of type A" << '\n';
+		return;
+	}
+	B* new_B = dynamic_cast<B*>(p);
+	if (new_B != NULL)
+	{
+		std::cout << "pointer is of type B" << '\n';
+		return;
+	}
+	C* new_C = dynamic_cast<C*>(p);
+	if (new_C != NULL)
+	{
+		std::cout << "pointer is of type C" << '\n';
+		return;
+	}
+}
+
+void identify(Base& p)
+{
+	try {
+		(void)dynamic_cast<A&>(p);
+		std::cout << "pointer is of type A" << '\n';
+		return;
+	}
+	catch (std::exception& e) {}
+
+	try {
+		(void)dynamic_cast<B&>(p);
+		std::cout << "pointer is of type B" << '\n';
+		return;
+	}
+	catch (std::exception& e) {}
+
+	try {
+		(void)dynamic_cast<C&>(p);
+		std::cout << "pointer is of type C" << '\n';
+		return;
+	}
+	catch (std::exception& e) {}
+}
 
 int main(void)
 {
 	Base*	pop;
 	pop = generate();
+
+	identify(pop);
+	identify(*pop);
 	delete pop;
 	return (0);
 }
