69e6bca7268ca496e62453788cd7c7e1ae4ec875
Gap <132090220+Gpatrix@users.noreply.github.com>
2025-02-24 13:47:48 +0100

    fix: CPP-7/ex01: added const variable

--- CPP-7/ex01/iter.hpp
+++ CPP-7/ex01/iter.hpp
@@ -1,9 +1,20 @@
 #pragma once
 #include <iostream>
 
+template <typename T> 
+void iter(const T* array, int len, void (*fonct)(const T&))
+{
+	int	index = 0;
+
+	while (index < len)
+	{
+		fonct(array[index]);
+		index++;
+	}
+}
 
 template <typename T> 
-void iter(T* array, int len, void (*fonct)(const T&))
+void iter(T* array, int len, void (*fonct)( T&))
 {
 	int	index = 0;
 
--- CPP-7/ex01/main.cpp
+++ CPP-7/ex01/main.cpp
@@ -2,10 +2,36 @@
 #include "iter.hpp"
 
 
-int main(void)
-{
-	int test[5] = {1, 2, 3, 4, 5};
+#include "iter.hpp"
 
-	::iter(test, 5, &display_iter);
-	return (0);
+class Awesome
+{
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
+}
+
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
+
+int main() {
+  int tab[] = { 0, 1, 2, 3, 4 };
+  Awesome tab2[5];
+
+  iter( tab, 5, print<const int> );
+  iter( tab2, 5, print<Awesome> );
+
+  return 0;
 }
